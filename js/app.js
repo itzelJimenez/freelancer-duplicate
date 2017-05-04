30d112db41d32932289ec006f3765cf4ef9d295a
Itzel <itzeljimenez.do@gmail.com>
2017-05-04 21:26:33 +0000

    funcionalidad de padding en nav-bar

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
  * Archivo principal de JS
  */
  var navBar = document.getElementById('nav');
-
+ var portfolio = document.getElementsByClassName("link")[0];
+ var about = document.getElementsByClassName("link")[1];
+ var contact = document.getElementsByClassName("link")[2];
 function showBar(){
   var countScroll = document.body.scrollTop;
     if(countScroll>120){
@@ -12,5 +14,23 @@ function showBar(){
     	navBar.removeAttribute("class", "nav-modal")
  		navBar.setAttribute("class", "nav-bar");
     }
+    if(countScroll >= 780 && countScroll<1512.9){
+    	portfolio.setAttribute("class", "padding");
+    } else{
+    	portfolio.removeAttribute("class", "padding")
+    }
+    if (countScroll >= 1512.9 && countScroll<1848.9) {
+    	about.setAttribute("class", "padding");
+    } else {
+    	about.removeAttribute("class", "padding")
+    }
+     if (countScroll >= 1848.9 && countScroll<2440.8) {
+    	contact.setAttribute("class", "padding");
+    } else {
+    	contact.removeAttribute("class", "padding")
+    }
+    console.log(document.body.scrollTop);
 }
+
+
  document.addEventListener("scroll", showBar);
