fd8dfde1ca753c785853b4f830b6d32284cbc864
Itzel <itzeljimenez.do@gmail.com>
2017-05-04 20:37:46 +0000

    nav-bar con funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 /*
  * Archivo principal de JS
  */
+ var navBar = document.getElementById('nav');
+
+function showBar(){
+  var countScroll = document.body.scrollTop;
+    if(countScroll>120){
+    	navBar.removeAttribute("class", "nav-bar")
+ 		navBar.setAttribute("class", "nav-modal");
+    } else {
+    	navBar.removeAttribute("class", "nav-modal")
+ 		navBar.setAttribute("class", "nav-bar");
+    }
+}
+ document.addEventListener("scroll", showBar);
