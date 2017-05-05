4f3609552e1a692998efcbb818550a1cf7d1c043
itzel <itzeljimenez.do@gmail.com>
2017-05-05 02:39:22 -0500

    funcionalidad de links, vista previa

--- js/app.js
+++ js/app.js
@@ -5,6 +5,14 @@
  var portfolio = document.getElementsByClassName("link")[0];
  var about = document.getElementsByClassName("link")[1];
  var contact = document.getElementsByClassName("link")[2];
+ var imgPortfolio = document.getElementsByClassName("images");
+ var modal =document.getElementById("modal");
+ var img = document.getElementById("imagen");
+ var x=document.getElementById("x");
+ var inputData=document.getElementsByClassName("placeholder");
+ var labelInfo=document.getElementsByClassName("label-hidden");
+
+
 function showBar(){
   var countScroll = document.body.scrollTop;
     if(countScroll>120){
@@ -29,8 +37,23 @@ function showBar(){
     } else {
     	contact.removeAttribute("class", "padding")
     }
-    console.log(document.body.scrollTop);
 }
-
-
+function imgPrev(){
+   img.src=this.src;
+   modal.style.display = "block";
+   console.log(this)
+}
+function cerrar(){
+    modal.style.display="none";
+}
+function showLabel(){
+    alert("a")
+}
+for(var i=0; i<imgPortfolio.length;i++){
+    imgPortfolio[i].addEventListener("click", imgPrev);
+}
+for(var i=0; i<inputData; i++){
+    inputData[i].addEventListener("keypress", showLabel);
+}
  document.addEventListener("scroll", showBar);
+ x.addEventListener("click", cerrar);
